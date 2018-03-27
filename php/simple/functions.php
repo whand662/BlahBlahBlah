6703f52386d9a5e5657a18001acb37ec33e4ecfb
LauraLucaciu <laura7977@yahoo.com>
2018-03-27 11:36:12 -0400

    Add files via upload
    Updated

--- php/simple/functions.php
+++ php/simple/functions.php
@@ -1,26 +1,20 @@
 <?php
+
   function show_posts($userid){
-    include("mysql_connection.php");
-  // //make connection
-  // $connect = mysqli_connect('localhost', 'root', '');
-  // //select db
-  // mysqli_select_db($connect, 'class_project');
-  //
-  // if(mysqli_connect_errno()){
-  //   exit();
-  // }
-    $posts = array();
+      include("mysql_connection.php");
 
-    $sql = "select body, post_time from twitts
+      $posts = array();
+
+      $sql = "select body, post_time from twitts
                   where uid = '$userid' order by post_time desc";
-    $result = mysqli_query($connect, $sql);
+      $result = mysqli_query($connect, $sql);
 
-    while($data = mysqli_fetch_object($result)){
-        $posts[] = array(   'post_time' => $data->post_time,
-                            'uid' => $userid,
-                            'body' => $data->body);
-    }
-    return $posts;
+      while($data = mysqli_fetch_object($result)){
+            $posts[] = array(   'post_time' => $data->post_time,
+                                'uid' => $userid,
+                                'body' => $data->body);
+      }
+      return $posts;
 
   }
 
@@ -34,5 +28,24 @@
           $users[$data->uid] = $data->username;
       }
       return $users;
+  }
+
+//show messages
+  function show_messages($userid){
+      include("mysql_connection.php");
+      $messages = array();
+
+      $sql = "SELECT sender_id, receiver_id, body, send_time FROM message
+                  WHERE receiver_id = '$userid' order by send_time desc";
+      $result = mysqli_query($connect, $sql);
+
+      while($data = mysqli_fetch_object($result)){
+            $messages[] = array(   'send_time' => $data->send_time,
+                                   'sender_id' => $data-> sender_id,
+                                   'uid' => $userid,
+                                   'body' => $data->body);
+      }
+    return $messages;
+
   }
 ?>
