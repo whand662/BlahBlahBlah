2509662e0ac5a4b75b833359e452e86d0c6a5e07
LauraLucaciu <laura7977@yahoo.com>
2018-03-29 11:23:07 -0400

    Add files via upload
    update q9

--- php/simple/Query9.php
+++ php/simple/Query9.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+
 <html>
   <head>
     <meta charset="UTF-8">
@@ -17,12 +17,10 @@
           margin-right: auto;
           width: 10%;
        }
-
       </style>
 
 
     <?php
-
       function add_comment(){
         $connect = mysqli_connect('localhost', 'root', '');
         $uid = $_POST["uid"];
@@ -32,26 +30,20 @@
         $result = mysqli_query($connect, $sql);
         $connect -> close();
       }
-
       include('mysql_connection.php');
       include('functions.php');
       //make connection
       $connect = mysqli_connect('localhost', 'root', '');
-
       //select db
       mysqli_select_db($connect, 'class_project');
-
       if(mysqli_connect_errno()){
         exit();
       }
-
       $connect -> close();
-
       $posts = all_posts_with_comments();
-
       if (count($posts)){
       foreach ($posts as $key => $list){
-          echo <table border='1' cellspacing='0' cellpadding='5' width='500'>;
+          echo "<table border='1' cellspacing='0' cellpadding='5' width='500'>";
           echo "<tr valign='top'>\n";
           echo "<td>".$list['uid'] ."</td>\n";
           echo "<td>".$list['body'] ."<br/>\n";
@@ -64,19 +56,18 @@
             echo "<small>".$sublist['comment_time'] ."</small></td>\n";
             echo "</tr>\n";
           }
-          echo <form action="Query5helper.php" method="POST">
-          echo  Enter a year: <input type="text" placeholder="Comment" id="comment" name="comment"/>
-          echo  Enter your userID: <input type="text" placeholder="ID" id="uid" name="uid"/>
-          echo  <input type="hidden" name="tid" value="$list['tid']" />
-          echo  <br></br>
-          echo  <input type="submit" name="submit">
-          echo </form>
-          echo </table>;
+          echo "<form action="Query9helper.php" method="POST">"
+          echo  "Enter a year: <input type="text" placeholder="Comment" id="comment" name="comment"/>"
+          echo  "Enter your userID: <input type="text" placeholder="ID" id="uid" name="uid"/>"
+          echo  "<input type="hidden" name="tid" value="$list['tid']" />"
+          echo  "<br></br>"
+          echo  "<input type="submit" name="submit">"
+          echo "</form>"
+          echo "</table>";
       }
       }else{
       echo <p><b>You havent posted anything yet!</b></p>;
       }
-
     ?>
 
   </head>
--- php/simple/Query9helper.php
+++ php/simple/Query9helper.php
@@ -1,29 +1,21 @@
-
 <?php
-
   function add_comment(){
-
   }
-
   include('mysql_connection.php');
   include('functions.php');
   //make connection
   $connect = mysqli_connect('localhost', 'root', '');
   mysqli_select_db($connect, 'class_project');
-
   if(mysqli_connect_errno()){
     header("Location: Query9.php");
     exit;
   }
-
   $uid = $_POST["uid"];
   $tid = $_POST["tid"];
   $body = $_POST["body"];
   $sql = "INSERT INTO comment (uid, tid, body) VALUES ('$uid', '$tid', '$body')";
   $result = mysqli_query($connect, $sql);
-
   $connect -> close();
-
   header("Location: Query9.php");
   exit;
 ?>
