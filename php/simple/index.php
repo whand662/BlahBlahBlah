c854bb773d99051b747cf89244bd92c346638a45
LauraLucaciu <laura7977@yahoo.com>
2018-04-04 14:08:30 -0400

    Cleaned up a little bit
    removed things that was not need it.

--- php/simple/index.php
+++ php/simple/index.php
@@ -11,13 +11,12 @@ include 'functions.php';
 
 </head>
 <body>
-  <p>Add new twitt:</p>
+  <p>Post new twitt:</p>
   <form method='POST' action="index.php?id=<?php echo $_SESSION["uid"]; ?>">
     <input type='text' name='combody' >
           <button type='submit' name='commentSubmit'>Post</button>
     </form>
 </body>
-
 
 </html>
 
@@ -34,14 +33,15 @@ if($dresult)
   if (count($posts)){
 ?>
 <?php
-
-   //echo "<h2>User ".$_POST["username"], " posted: </h2>";
+    
+    echo "<p>  </p> ";
+    echo "<b>New Twitt:</b>";
+    
   foreach ($posts as $key => $list){
       echo "<tr valign='top'>\n";
-    //  echo "<td>".$_POST["username"] ."</td>\n";
       echo "<td>".$list['body'] ."<br/>\n";
       echo "<small>".$list['post_time'] ."</small></td>";
-    //  echo "</tr>\n";
+      echo "</tr>\n";
     }
 
 }else{
@@ -54,19 +54,11 @@ if(isset($_POST['commentSubmit'])){
 
   //query to post new twitt
   $sql = "insert INTO twitts VALUES (null,'$uid', '$body',NOW())";
-  //echo $sql;
-
+  
   $result = mysqli_query($connect, $sql);
   if($result){
-    echo "<p></p>";
-    echo "<b>User ".$_GET["id"], " posted a new twitt: ";
-
-  header("Location: index.php");
-  // //  echo "<b>User ".$_GET["id"], " posted a new twitt: ";
-  //   echo "</b>";
-  //   //  echo "<br>User posted new twitt.</br>";
+      header("Location: index.php");
   }
-
 
 }
 ?>
