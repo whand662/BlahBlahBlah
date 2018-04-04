69cf3712e84bc93525d9d34cd218f4c182581f66
LauraLucaciu <laura7977@yahoo.com>
2018-04-04 15:07:04 -0400

    clean up

--- php/simple/loginQ6.php
+++ php/simple/loginQ6.php
@@ -1,4 +1,3 @@
-
 <?php
   include("mysql_connection.php");
   include('functions.php');
@@ -6,7 +5,7 @@
   if(isset(($_POST['submit']))){
     //check if the user and password are empty
     if(empty($_POST['username']) || empty($_POST['password'])){
-      echo "<b>Please enter Username and Password to log in!</b>";
+      echo "Enter both fields for Username and Password!";
     }else{
       //define user and password
       $username=$_POST['username'];
@@ -27,11 +26,11 @@
       if($rows['username']== $username && $rows['password']== $password){
           echo "<b>Welcome ".$rows['username'],"!!!</b>"," "," You are now logged in";
       }else{
-       echo "<b>Username or Password doesn't match!</b>";
+       echo "Username or Password are invalid. Please try again!!!";
         }
-              // user receives messages query 6
+              // user receives messages
               session_start();
-
+              //fetch data
               $sql = "SELECT uid, username, sender_id from user, message where username=\"".$_POST["username"]."\"";
               $result = mysqli_query($connect, $sql);
               if($result)
@@ -49,15 +48,14 @@
 
                     foreach ($messages as $key => $list){
                         echo "<tr valign='top'>\n";
-                        //echo "<td>".$list['uid'] ."</td>\n";
                         echo "<td> Receiver: ".$_POST["username"] ."</td>\n";
-                        $temp="select username from user where uid=".$list['sender_id'];
-                        $temp_r=mysqli_query($connect,$temp);
-                        $temp_f=mysqli_fetch_assoc($temp_r);
-                        echo "<td>".$temp_f["username"] ."</td>\n";
-                        echo "<td>".$list['body'] ."<br/>\n";
-                        echo "<small>".$list['send_time'] ."</small></td>\n";
-                        echo "</tr>\n";
+                                   $temp="select username from user where uid=".$list['sender_id'];
+                                   $temp_r=mysqli_query($connect,$temp);
+                                   $temp_f=mysqli_fetch_assoc($temp_r);
+                                        echo "<td>".$temp_f["username"] ."</td>\n";
+                                        echo "<td>".$list['body'] ."<br/>\n";
+                                        echo "<small>".$list['send_time'] ."</small></td>\n";
+                                        echo "</tr>\n";
                       }
                 ?>
               </table>
